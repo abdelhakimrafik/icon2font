f3815be444a4cf100acb2f5bdcf08cb30e0f5b3e
Abdelhakim RAFIK <ra.abdelhakim@gmail.com>
2024-09-29 14:55:06 +0100

    refacto: updated vite config files

--- vite.config.plugin.ts
+++ vite.config.plugin.ts
@@ -4,32 +4,35 @@ import generateFile from 'vite-plugin-generate-file';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 import figmaManifest from './manifest';
 
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    viteSingleFile(),
-    generateFile({
-      type: 'json',
-      output: './manifest.json',
-      data: figmaManifest
-    })
-  ],
-  build: {
-    minify: mode === 'production',
-    sourcemap: mode !== 'production' ? 'inline' : false,
-    target: 'es2017',
-    emptyOutDir: false,
-    outDir: path.resolve('dist'),
-    rollupOptions: {
-      input: path.resolve('src/plugin/index.ts'),
-      output: {
-        entryFileNames: 'plugin.js'
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+  return {
+    plugins: [
+      viteSingleFile(),
+      generateFile({
+        type: 'json',
+        output: './manifest.json',
+        data: figmaManifest
+      })
+    ],
+    build: {
+      minify: isProduction,
+      sourcemap: !isProduction ? 'inline' : false,
+      target: 'esnext',
+      emptyOutDir: false,
+      outDir: path.resolve('dist'),
+      rollupOptions: {
+        input: path.resolve('src/plugin/index.ts'),
+        output: {
+          entryFileNames: 'plugin.js'
+        }
+      }
+    },
+    resolve: {
+      alias: {
+        '@common': path.resolve('src/common'),
+        '@plugin': path.resolve('src/plugin')
       }
     }
-  },
-  resolve: {
-    alias: {
-      '@common': path.resolve('src/common'),
-      '@plugin': path.resolve('src/plugin')
-    }
-  }
-}));
+  };
+});
--- vite.config.ui.ts
+++ vite.config.ui.ts
@@ -1,26 +1,29 @@
 import path from 'node:path';
 import { defineConfig } from 'vite';
-import { viteSingleFile } from 'vite-plugin-singlefile';
 import react from '@vitejs/plugin-react';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
-export default defineConfig(({ mode }) => ({
-  plugins: [react(), viteSingleFile()],
-  root: path.resolve('src/ui'),
-  build: {
-    minify: mode === 'production',
-    cssMinify: mode === 'production',
-    sourcemap: mode !== 'production' ? 'inline' : false,
-    emptyOutDir: false,
-    cssCodeSplit: false,
-    outDir: path.resolve('dist'),
-    rollupOptions: {
-      input: path.resolve('src/ui/index.html')
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+  return {
+    plugins: [react(), viteSingleFile()],
+    root: path.resolve('src/ui'),
+    build: {
+      minify: isProduction,
+      cssMinify: isProduction,
+      sourcemap: !isProduction ? 'inline' : false,
+      emptyOutDir: false,
+      cssCodeSplit: false,
+      outDir: path.resolve('dist'),
+      rollupOptions: {
+        input: path.resolve('src/ui/index.html')
+      }
+    },
+    resolve: {
+      alias: {
+        '@common': path.resolve('src/common'),
+        '@ui': path.resolve('src/ui')
+      }
     }
-  },
-  resolve: {
-    alias: {
-      '@common': path.resolve('src/common'),
-      '@ui': path.resolve('src/ui')
-    }
-  }
-}));
+  };
+});
